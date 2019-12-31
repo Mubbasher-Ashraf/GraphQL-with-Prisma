73c12fe04249ee60cd91bbb81a85470af0cdba1a
Mubbasher Ashraf <55390359+Mubbasher-Ashraf@users.noreply.github.com>
2019-12-31 18:05:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,60 +16,6 @@ const resolvers = {
      Vote,
 }
 
-// const resolvers = {
-//      Query: {
-//           info: () => 'Enter Info of typeDefs',
-//           feed: (parent, args, context, info) => {
-//                return context.prisma.links()
-//           },
-
-//           // link: (parent, args, context, info) =>{
-//           //      let updated = links.filter((item) =>{
-//           //           return item.id === args.id
-//           //      });
-
-//           //      return updated[0];
-//           // },
-//      },
-
-//      Mutation: {
-//           post: (parent, args , context) =>{
-//                return context.prisma.createLink({
-//                     url: args.url,
-//                     description: args.description,
-//                });
-//                // const link = {
-//                //      id: `link-${idCount++}`,
-//                //      description: args.description,
-//                //      url: args.url
-//                // }
-
-//                // links.push(link);
-//                // return link;
-//           },
-
-//           // updateLink: (parent, args) =>{
-//           //      let updated = links.filter((item) =>{
-//           //           return item.id === args.id
-//           //      });
-
-//           //      let index = links.indexOf(updated[0]);
-//           //      let obj = {id: args.id, url: args.url, description: args.description}
-//           //      links.splice(index, 1, obj);
-//           //      return args;
-//           // },
-
-//           // deleteLink: (parent, args) =>{
-//           //      let updated = links.filter((item) =>{
-//           //           return item.id === args.id
-//           //      });
-//           //      let index = links.indexOf(updated[0]);
-//           //      links.splice(index, 1);
-//           //      return updated[0];
-//           // },
-//      }
-// }
-
 const server = new GraphQLServer({
      // typeDefs,
      typeDefs: './schema.graphql',
